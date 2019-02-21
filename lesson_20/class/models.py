284e4404ed257d11cfcd3beb40150199e7f3a93a
Mario Beltrán Alarcón <belco90@gmail.com>
2019-02-21 11:53:09 +0100

    Lesson 20: Class examples

---
+++ lesson_20/class/models.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+from google.appengine.ext import ndb
+
+
+class Message(ndb.Model):
+    content = ndb.StringProperty()
+    created_at = ndb.DateTimeProperty(auto_now_add=True)
+    deleted = ndb.BooleanProperty(default=False)  # flag para saber si está borrado o no
